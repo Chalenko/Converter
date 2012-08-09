e65742bf398d014f48077619ca7c2f9b41de6206
Pavel <chalenkop@gmail.com>
2012-08-09 17:15:27 +0400

    Update GUI

--- Converter/src/Converter/Frame.java
+++ Converter/src/Converter/Frame.java
@@ -4,24 +4,49 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Frame {
-	public static void Create(){
+	public static void Create() {
 		JFrame jf = new JFrame("Converter");
 		jf.setBounds(312, 234, 400, 300);
 		jf.setLayout(new FlowLayout());
 		Container cp = jf.getContentPane();
-		JRadioButton jRadioButtonFromCelsium = new JRadioButton("Celsium",false);
-		JRadioButton jRadioButtonFromKelvin = new JRadioButton("Kelvin",false);
-		JRadioButton jRadioButtonToCelsium = new JRadioButton("Celsium",false);
-		JRadioButton jRadioButtonToKelvin = new JRadioButton("Kelvin",false);
-		
+
+		Panel p1 = new Panel();
+		Panel p2 = new Panel();
+
+		p1.setLayout(new GridLayout(3, 1));
+		p2.setLayout(new GridLayout(3, 1));
+
+		JRadioButton jRadioButtonFromCelsium = new JRadioButton("Celsium",
+				false);
+		JRadioButton jRadioButtonFromKelvin = new JRadioButton("Kelvin", false);
+		JRadioButton jRadioButtonFromFahrenheit = new JRadioButton(
+				"Fahrenheit", false);
+
+		JRadioButton jRadioButtonToCelsium = new JRadioButton("Celsium", false);
+		JRadioButton jRadioButtonToKelvin = new JRadioButton("Kelvin", false);
+		JRadioButton jRadioButtonToFahrenheit = new JRadioButton("Fahrenheit",
+				false);
+
 		ButtonGroup buttonGroupFrom = new ButtonGroup();
 		ButtonGroup buttonGroupTo = new ButtonGroup();
-		
-		buttonGroupFrom.add(jRadioButtonFromCelsium); buttonGroupFrom.add(jRadioButtonFromKelvin);
-		buttonGroupTo.add(jRadioButtonToCelsium); buttonGroupTo.add(jRadioButtonToKelvin);
-		
-		cp.add(jRadioButtonFromCelsium); cp.add(jRadioButtonFromKelvin); cp.add(jRadioButtonToCelsium); cp.add(jRadioButtonToKelvin);
+
+		buttonGroupFrom.add(jRadioButtonFromCelsium);
+		buttonGroupFrom.add(jRadioButtonFromKelvin);
+		buttonGroupFrom.add(jRadioButtonFromFahrenheit);
+		buttonGroupTo.add(jRadioButtonToCelsium);
+		buttonGroupTo.add(jRadioButtonToKelvin);
+		buttonGroupTo.add(jRadioButtonToFahrenheit);
+
+		p1.add(jRadioButtonFromCelsium);
+		p1.add(jRadioButtonFromKelvin);
+		p1.add(jRadioButtonFromFahrenheit);
+		p2.add(jRadioButtonToCelsium);
+		p2.add(jRadioButtonToKelvin);
+		p2.add(jRadioButtonToFahrenheit);
+
+		jf.setLayout(new BorderLayout());
+		cp.add(p1, BorderLayout.WEST);
+		cp.add(p2, BorderLayout.EAST);
 		jf.setVisible(true);
 	}
 }
-
